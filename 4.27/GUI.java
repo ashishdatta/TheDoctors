d14d6beb470bd1096c79c74221e591a12775292b
ashishbdatta <ashishbdatta@gmail.com>
2013-04-28 20:13:35 -0700

    updated the option for users

--- 4.27/GUI.java
+++ 4.27/GUI.java
@@ -6,28 +6,27 @@ public class GUI
 	{
 		Database info = new Database();
 		Medical val = new Medical();
-		String title, bloodPressure, patientFirstName, patientSurname, weight, sugarLevel;  
+		String title, bloodPressure, patientFirstName, patientSurname, weight, sugarLevel, user;  
 		int continueProgram = 0;
 		
-		JFrame home = new JFrame("Well Check Clinic");
-		home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		home.pack();
-		home.setVisible(true);
+		user = JOptionPane.showInputDialog("Please enter your occupation: (Doctor, Nurse or Patient");
 		do
 		{
-			patientFirstName = JOptionPane.showInputDialog("Patient First Name: ");
-			patientSurname = JOptionPane.showInputDialog("Patient Last Name: ");
-			bloodPressure = JOptionPane.showInputDialog("Blood Pressure: (Use ' . ' instead of ' / ')");
-			weight = JOptionPane.showInputDialog("Weight: ");
-			sugarLevel = JOptionPane.showInputDialog("Sugar Level: ");
-			
-			
-			continueProgram = JOptionPane.showConfirmDialog(null, "Add another patient?");
-			info.addPatient(patientFirstName, patientSurname);
-			info.addMedicalData(Double.parseDouble(weight), bloodPressure, Integer.parseInt(sugarLevel));
-			
-		} while(continueProgram == JOptionPane.YES_OPTION);
-		
+			do
+			{
+				patientFirstName = JOptionPane.showInputDialog("Patient First Name: ");
+				patientSurname = JOptionPane.showInputDialog("Patient Last Name: ");
+				bloodPressure = JOptionPane.showInputDialog("Blood Pressure: (Use ' . ' instead of ' / ')");
+				weight = JOptionPane.showInputDialog("Weight: ");
+				sugarLevel = JOptionPane.showInputDialog("Sugar Level: ");
+				
+				
+				continueProgram = JOptionPane.showConfirmDialog(null, "Add another patient?");
+				info.addPatient(patientFirstName, patientSurname);
+				info.addMedicalData(Double.parseDouble(weight), bloodPressure, Integer.parseInt(sugarLevel));
+				
+			}while(continueProgram == JOptionPane.YES_OPTION);
+		}while(user == "Nurse".toLowerCase());
 		JOptionPane.showMessageDialog(null, info.toString());
 		
 		
